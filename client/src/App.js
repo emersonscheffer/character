3e618476eb022042935c7de29d7cf5eb1e3ee54f
Emerson <captainscheffer@gmail.com>
2021-06-29 19:30:44 -0700

    initial upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import ActivityBar from "./components/ActivityBar";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ActivityBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ActivityBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ActivityBar = () => {
+  return (
+    <div>
+      <h1>ActivityBar</h1>
+    </div>
+  );
+};
+
+export default ActivityBar;
