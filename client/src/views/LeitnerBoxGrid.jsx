a53fed93639e371e41f9a11129ac47d5fe078bd2
emersonscheffer <32528159+emersonscheffer@users.noreply.github.com>
2021-11-20 20:25:10 -0800

    upgrade grid layout

--- client/src/views/LeitnerBoxGrid.jsx
+++ client/src/views/LeitnerBoxGrid.jsx
@@ -7,17 +7,26 @@ const LeitnerBoxGrid = () => {
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "repeat(" + cols + ", 1fr)",
-        gridTemplateRows: "25px 90px 65px auto",
-        gridTemplateAreas:"header header header header"
+        // gridTemplateColumns: "repeat(" + cols + ", 1fr)",
+        // gridTemplateRows: "25px 90px 65px auto",
+        // gridTemplateAreas:"header header header header"
+        gridTemplate: `
+          "stripe stripe stripe" 25px
+          "timeline timeline timeline" 90px
+          "leftside control rightside" 50px
+          "leftside canvas rightside" auto
+          "footer footer footer" 25px
+          / 1fr 3fr 1fr
+        `,
       }}
     >
       <div
         style={{
           backgroundColor: "gray",
           //height: "25px",
-          gridColumn: "1/" + (cols + 1),
-          gridRow: "1/2",
+          //gridColumn: "1/" + (cols + 1),
+          //gridRow: "1/2",
+          gridArea: "stripe",
         }}
       >
         first stripe
@@ -27,8 +36,9 @@ const LeitnerBoxGrid = () => {
         style={{
           backgroundColor: "blue",
           //height: "90px",
-          gridColumn: "1/" + (cols + 1),
-          gridRow: "2/3",
+          //gridColumn: "1/" + (cols + 1),
+          //gridRow: "2/3",
+          gridArea: "timeline",
         }}
       >
         timeline
@@ -38,8 +48,9 @@ const LeitnerBoxGrid = () => {
         style={{
           backgroundColor: "green",
           //height: "250px",
-          gridColumn: "1/" + 2,
-          gridRow: "3/4",
+          //gridColumn: "1/" + 2,
+          //gridRow: "3/4",
+          gridArea: "leftside",
         }}
       >
         side left panel current deck name / SQ
@@ -49,8 +60,9 @@ const LeitnerBoxGrid = () => {
         style={{
           backgroundColor: "pink",
           //height: "90px",
-          gridColumn: "2/8",
-          gridRow: "3/4",
+          //gridColumn: "2/8",
+          //gridRow: "3/4",
+          gridArea: "control",
         }}
       >
         central controller / study button
@@ -59,9 +71,10 @@ const LeitnerBoxGrid = () => {
       <div
         style={{
           backgroundColor: "red",
-          height: "auto",
-          gridColumn: "2/8",
-          gridRow: "4",
+          height: "400px",
+          //gridColumn: "2/8",
+          //gridRow: "4",
+          gridArea: "canvas",
         }}
       >
         canvas
@@ -70,13 +83,15 @@ const LeitnerBoxGrid = () => {
       <div
         style={{
           backgroundColor: "white",
-          height: "auto",
-          gridColumn: "8/9",
-          gridRow: "3/4",
+          //height: "auto",
+          //gridColumn: "8/9",
+          //gridRow: "3/4",
+          gridArea: "rightside",
         }}
       >
         right side / list
       </div>
+      <div style={{ backgroundColor: "grey", gridArea: "footer" }}>footer</div>
     </div>
   );
 };
