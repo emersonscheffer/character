eba4c04c2341848d0d1de3c50c1e83d082ed8626
emersonscheffer <32528159+emersonscheffer@users.noreply.github.com>
2021-12-05 19:23:35 -0800

    fix

--- server.js
+++ server.js
@@ -1,70 +1,93 @@
-// temp file, just to get the date()
+// // temp file, just to get the date()
 
-//import "./cards.json"
+// //import "./cards.json"
 
-console.log(" - - - - - - ");
+// console.log(" - - - - - - ");
 
-const dataX = require("../character/client/src/cardsData.json");
-//console.log(dataX[0]);
+// const dataX = require("../character/client/src/cardsData.json");
+// //console.log(dataX[0]);
 
-// const da = new Date
+// // const da = new Date
 
-// da.getDate()
+// // da.getDate()
 
-// console.log(da.getMonth() + 1, da.getDate())
+// // console.log(da.getMonth() + 1, da.getDate())
 
-console.log(" - - - - - - ");
+// console.log(" - - - - - - ");
 
-class MyQueue {
-  constructor() {
-    this.store = [];
-  }
-  add(card) {
-    this.store.push(card);
-  }
-  isEmpty() {
-    return this.store.length < 1 ? true : false;
-  }
+// class MyQueue {
+//   constructor() {
+//     this.store = [];
+//   }
+//   add(card) {
+//     this.store.push(card);
+//   }
+//   isEmpty() {
+//     return this.store.length < 1 ? true : false;
+//   }
 
-  peek() {
-    return !this.isEmpty() ? this.store[0] : null;
-  }
+//   peek() {
+//     return !this.isEmpty() ? this.store[0] : null;
+//   }
 
-  dequeue() {
-    return !this.isEmpty() ? this.store.shift() : "empty";
-  }
+//   dequeue() {
+//     return !this.isEmpty() ? this.store.shift() : "empty";
+//   }
+// }
+
+// let current = new MyQueue();
+// current.add(dataX[0]);
+// current.add(dataX[1]);
+// current.add(dataX[2]);
+
+// const showCard = () => {
+//   let card = current.peek();
+
+//   console.log("== == = == === == Show Card == = === == == ");
+
+//   console.log(card.title);
+
+//   console.log("== == = == === == Show Card == = === == == ");
+// };
+
+// showCard();
+
+// const pressBtn = (answer) => {
+//   const card = current.dequeue();
+//   if (answer === "good") {
+//   } else {
+//     current.add(card);
+//   }
+// };
+
+// pressBtn("good");
+
+// console.log(current);
+
+// showCard();
+// pressBtn("no");
+
+// console.log(current);
+
+let current = []
+
+const box1 = [1, 2, 3, 4]
+const box2 = [32, 34, 35]
+
+const allBoxes = [box1, box2]
+
+function checkBox(b){
+  if(b < 1) {
+    return
+  } 
+  current = [...current, ...allBoxes[b - 1]]
+  checkBox(b - 1)
 }
 
-let current = new MyQueue();
-current.add(dataX[0]);
-current.add(dataX[1]);
-current.add(dataX[2]);
+checkBox(allBoxes.length)
+// let box = []
+// checkBox(box + 1)
 
-const showCard = () => {
-  let card = current.peek();
 
-  console.log("== == = == === == Show Card == = === == == ");
 
-  console.log(card.title);
-
-  console.log("== == = == === == Show Card == = === == == ");
-};
-
-showCard();
-
-const pressBtn = (answer) => {
-  const card = current.dequeue();
-  if (answer === "good") {
-  } else {
-    current.add(card);
-  }
-};
-
-pressBtn("good");
-
-console.log(current);
-
-showCard();
-pressBtn("no");
-
-console.log(current);
+console.log(current)
